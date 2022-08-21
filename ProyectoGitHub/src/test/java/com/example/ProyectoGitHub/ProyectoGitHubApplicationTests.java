3761f570f98ebf61837c24c07454814be41558bd
JhonatanChavarroarias <110508336+JhonatanChavarroArias@users.noreply.github.com>
2022-08-20 21:51:24 -0500

    Primer Comentario
    primera configuración de git

---
+++ ProyectoGitHub/src/test/java/com/example/ProyectoGitHub/ProyectoGitHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ProyectoGitHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoGitHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
